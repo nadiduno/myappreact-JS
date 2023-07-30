0bec5eba316b97858657bedc4ff15cfd78989fef
Nadi Duno <nadiduno@gmail.com>
2023-07-29 22:45:56 -0300

    Rendering DOC

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-function Exemplo(){
-    return(
+function Exemplo() {
+    return (
         <div>
-
+            <h1>REACT</h1>
+            <p>assim trabalha</p>
         </div>
     )
 }
 
-ReactDOM
+ReactDOM.render(<Exemplo/>, document.getElementById("root"))
