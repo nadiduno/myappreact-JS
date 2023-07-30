c8e71fc246b21a520a09eb8ba13af2cddaf5560f
Nadi Duno <nadiduno@gmail.com>
2023-07-29 22:16:22 -0300

    Setup project

---
+++ README.md
@@ -0,0 +1,11 @@
+#NPM
+https://www.npmjs.com/
+
+#REACT
+https://react.dev/
+
+``` Bash
+npm init -y
+npm i react
+npm i react-dom
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Exemplo(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+ReactDOM
